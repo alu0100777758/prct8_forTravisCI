9a640419946f32b035094049204b529347ee5f1d
alu0100764666 <alu0100764666@ull.edu.es>
2014-11-06 17:09:42 +0000

    Resuelta expectativa de existir pregunta true o false

--- lib/simpleselect.rb
+++ lib/simpleselect.rb
@@ -2,6 +2,7 @@ require "simpleselect/version"
 require "simpleselect/simpleSelect"
 require "simpleselect/listNode"
 require "simpleselect/list"
+require "simpleselect/tof"
 module Simpleselect
   # Your code goes here...
 end
